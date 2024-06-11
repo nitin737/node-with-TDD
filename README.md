bd2ab2e8b79e278edeafeaac8bef1763a6f9b6d3
nitin737 <nitin127001@gmail.com>
2024-06-11 10:54:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-with-TDD
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nitin737/node-with-TDD)
---
+++ index.spec.ts
@@ -0,0 +1,7 @@
+//import { add } from '../sample';
+
+describe('Hello TO TDD', () => {
+  it('should add two numbers correctly', () => {
+    console.log('Write your test');
+  });
+});
